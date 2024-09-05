8610e1460d284535272b9b8814a1f4d8407f24b8
Dami <166107033+23DP2DGeid@users.noreply.github.com>
2024-09-05 09:49:47 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,25 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello RVT!");
+        Scanner scanner = new Scanner(System.in);
 
+        String name;
+        String surname;
+        String group;
+        System.out.println("Ievade savu vārdu: ");
+        name=scanner.nextLine();
+        System.out.println("Ievade savu uzvārdu: ");
+        surname = scanner.nextLine();
+        System.out.println("Ievade savu grupu: ");
+        group = scanner.nextLine();
+
+        
+        System.out.println("Jūsu atbilde: ");
+        System.out.println(name + " " + surname+ " " + group);
+    scanner.close();
     }
 
 }
