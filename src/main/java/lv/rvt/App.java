4bfbd9e350fce65ffeac35bdc337f4ae7c30dbf9
Dami <166107033+23DP2DGeid@users.noreply.github.com>
2024-09-18 08:20:22 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,17 +7,49 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int second = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give points [0-100]:");
+        int points = Integer.valueOf(scanner.nextLine());
+        if (points < 0)
+        {
+            System.out.println("Grade: Impossible");
 
+        }
+        else if (points > 0 && points < 50 )
+        {
+            System.out.println("Grade: failed");
 
-        System.out.println(  first + "+" + second + "=" + (first +  second)   );
-        System.out.println(  first + "-" + second + "=" + (first -  second)  );
-        System.out.println(  first + "*" + second + "=" + (first *  second)  );
-        System.out.println( first + "/" + second + "=" + (first /  second)  );
-    
+        }
+        else if (points >=  50 && points < 60 )
+        {
+            System.out.println("Grade: 1");
+
+        }
+        else if (points >= 60 && points < 70 )
+        {
+            System.out.println("Grade: 2");
+
+        }
+        else if (points >= 70 && points < 80 )
+        {
+            System.out.println("Grade: 3");
+
+        }
+        else if (points >= 80 && points < 90 )
+        {
+            System.out.println("Grade: 4");
+
+        }
+        else if (points >= 90 && points <= 100 )
+        {
+            System.out.println("Grade: 5");
+
+        }
+        else if (points > 100 )
+        {
+            System.out.println("Grade: incredible!");
+
+        }
+
 
         scanner.close();
     }
