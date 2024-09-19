940a2ab6801ad1193f4b14b58d178eb409e23e8a
Dami <166107033+23DP2DGeid@users.noreply.github.com>
2024-09-19 08:06:11 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,48 +7,40 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give points [0-100]:");
-        int points = Integer.valueOf(scanner.nextLine());
-        if (points < 0)
+        System.out.println("Value of the gift?");
+        int value = Integer.valueOf(scanner.nextLine());
+        if (value < 5000)
         {
-            System.out.println("Grade: Impossible");
+            System.out.println("No tax!");
 
         }
-        else if (points > 0 && points < 50 )
+        else if (value >= 5000 && value < 25000)
         {
-            System.out.println("Grade: failed");
+            System.out.println("Tax:" + (100 + (value-5000)*0.08));
 
         }
-        else if (points >=  50 && points < 60 )
+        else if (value >= 25000 && value < 55000)
         {
-            System.out.println("Grade: 1");
+            System.out.println("Tax:" + (1700 + (value-25000)*0.1));
 
         }
-        else if (points >= 60 && points < 70 )
+        else if (value >= 55000 && value < 200000)
         {
-            System.out.println("Grade: 2");
+            System.out.println("Tax:" + (4700 + (value-55000 )*0.12));
 
         }
-        else if (points >= 70 && points < 80 )
+        else if (value >= 200000 && value < 1000000)
         {
-            System.out.println("Grade: 3");
+            System.out.println("Tax:" + (22100 + (value-200000)*0.15));
 
         }
-        else if (points >= 80 && points < 90 )
+        else if (value >= 1000000)
         {
-            System.out.println("Grade: 4");
+            System.out.println("Tax:" + (142100 + (value-1000000)*0.17));
 
         }
-        else if (points >= 90 && points <= 100 )
-        {
-            System.out.println("Grade: 5");
+        
 
-        }
-        else if (points > 100 )
-        {
-            System.out.println("Grade: incredible!");
-
-        }
 
 
         scanner.close();
