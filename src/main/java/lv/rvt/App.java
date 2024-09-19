6b532688569ec3176674d6ec93b2b458f36ffae6
Dami <166107033+23DP2DGeid@users.noreply.github.com>
2024-09-19 10:22:07 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,43 +7,30 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Value of the gift?");
-        int value = Integer.valueOf(scanner.nextLine());
-        if (value < 5000)
-        {
-            System.out.println("No tax!");
+        
+        while (true) {
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
 
-        }
-        else if (value >= 5000 && value < 25000)
-        {
-            System.out.println("Tax:" + (100 + (value-5000)*0.08));
+            if (number<0) {
+                System.out.println("Unsuitable number: ");
+                continue;
+                
+            }
 
+            else if (number == 0) {
+                break;
+                
+            }
+            else{
+                System.out.println(number*number);
+            }
         }
-        else if (value >= 25000 && value < 55000)
-        {
-            System.out.println("Tax:" + (1700 + (value-25000)*0.1));
+ 
 
-        }
-        else if (value >= 55000 && value < 200000)
-        {
-            System.out.println("Tax:" + (4700 + (value-55000 )*0.12));
-
-        }
-        else if (value >= 200000 && value < 1000000)
-        {
-            System.out.println("Tax:" + (22100 + (value-200000)*0.15));
-
-        }
-        else if (value >= 1000000)
-        {
-            System.out.println("Tax:" + (142100 + (value-1000000)*0.17));
-
-        }
         
 
 
-
-        scanner.close();
     }
 }
 
