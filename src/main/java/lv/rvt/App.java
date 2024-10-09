ee0cdc25ee78c326a2f9c25858d30c344bc2fba1
Dami <166107033+23DP2DGeid@users.noreply.github.com>
2024-10-09 08:14:18 +0000

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,36 +6,12 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        int hundred = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Enter range: From how much");
-
-        int range = Integer.valueOf(scanner.nextLine());
-
-        System.out.println("To how much");
-
-        int range2 = Integer.valueOf(scanner.nextLine());
-
-
-            divisibleByThreeInRange(range, range2);
-         
-
-
-        scanner.close();
-
-    }
-
-
-    public static void divisibleByThreeInRange(int beginning, int end){
-
-        for(int reiz = beginning; end + 1 > reiz; reiz++){
-
-            if (reiz % 3 == 0) {
-                System.out.println(reiz);
-                
-            }
-
-        } 
-        
-
+        while (hundred<=100) {
+            System.out.println(hundred);
+            hundred++;
+        }
+scanner.close();
     }
 }
