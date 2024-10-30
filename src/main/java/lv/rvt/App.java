9ca9bd19d788f06b1706df987f66ed33b8e8cdaf
Dami <166107033+23DP2DGeid@users.noreply.github.com>
2024-10-30 10:35:24 +0000

    Resetting the counter value

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,10 +7,11 @@ public class App
     public static void main(String[] args)
     {
         Product nam = new Product("banan",1.3, 5 );
-        decreasing dec = new decreasing(2);
+        decreasing dec = new decreasing(100);
         //nam.printProduct();
         dec.printValue();
         dec.decrement();
+        dec.reset();
         dec.printValue();
         dec.decrement();
         dec.printValue();
