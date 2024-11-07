a7d265da29f7f3be4cd4b852554f7957ac2ceff1
Dami <166107033+23DP2DGeid@users.noreply.github.com>
2024-11-07 09:14:30 +0000

    Statistics (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,15 +6,30 @@ public class App
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in); 
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
-        System.out.println("Sum: " + statistics.sum());
-        System.out.println("Average: " + statistics.average());
+        int number = 0;
+        int even = 0;
+        int odd = 0;
+        while (number != -1) 
+        {
+            statistics.addNumber(number);
+            if (number % 2 == 0) {
+                even += number;
+            }
+            else{
+                odd += number;
+            }
+            number = scan.nextInt();
 
+            
+        }
+        
+        
+
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + even);
+        System.out.println("Sum of odd numbers: " + odd);
     }
     
 }
